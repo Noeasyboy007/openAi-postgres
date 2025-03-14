41d2beddc605dc935d97564ec70a3d2f98c1a975
aritra bera <aritra.bera@lawsikho.in>
2025-03-15 02:16:10 +0530

    drizzal migration setup

--- db/index.js
+++ db/index.js
@@ -2,5 +2,3 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
 const db = drizzle(process.env.DATABASE_URL);
-
-// DATABASE_URL=postgresql://postgres:aritrabera007@localhost:5431/postgres
