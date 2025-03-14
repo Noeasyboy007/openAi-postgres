35c53b75c8daba84d86e3fb86743546b0d05494f
aritra bera <aritra.bera@lawsikho.in>
2025-03-15 01:20:09 +0530

    drizzal-setup

---
+++ db/schema.js
@@ -0,0 +1,8 @@
+import { integer, pgTable, text } from "drizzle-orm/pg-core";
+
+export const todosTable = pgTable("todos", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    todo: text().notNull(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+    updatedAt: timestamp('updated_at').notNull().$onUpdate(() => new Date()),
+});
